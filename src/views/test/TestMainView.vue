4302c1e34ca26c45784379781317331a8fd52184
0tak2 <0tak2.code@gmail.com>
2023-04-22 09:45:36 +0900

    4/21 input 태그를 이용한 사진 촬영 테스트 코드 작성

--- src/views/test/TestMainView.vue
+++ src/views/test/TestMainView.vue
@@ -1,14 +1,18 @@
 <script setup>
-import VideoRecord from '@/components/test/VideoRecord.vue';
+import ImageTake from '@/components/test/ImageTake.vue';
 import AudioRecord from '@/components/test/AudioRecord.vue';
+import ImageTakeUsingInput from '@/components/test/ImageTakeUsingInput.vue';
 
 </script>
 
 <template>
     <h1>꾹꾹 기술검증</h1>
     <h2>카메라 사용 테스트</h2>
-    <video-record />
+    <image-take />
     
     <h2>마이크 사용 테스트</h2>
     <audio-record />
+
+    <h2>디바이스 자체 Input 태그를 통한 미디어 캡쳐 및 업로드</h2>
+    <image-take-using-input />
 </template>
